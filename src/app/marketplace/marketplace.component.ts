e0fa456be0dfdd7f1ef725118ed0fb598bf3f913
Mariia <mariiapopovych@gmail.com>
2019-06-18 23:34:15 -0700

    add DB

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit} from '@angular/core';
-import {Album } from '../album.model';
+import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
@@ -11,7 +12,7 @@ import { AlbumService } from '../album.service';
 })
 export class MarketplaceComponent implements OnInit {
 
-  albums: Album[];
+  albums: FirebaseListObservable<any[]>;
 
   constructor(private router: Router, private albumService: AlbumService){}
 
@@ -19,9 +20,9 @@ export class MarketplaceComponent implements OnInit {
     this.albums = this.albumService.getAlbums();
   }
 
-  goToDetailPage(clickedAlbum: Album) {
-   this.router.navigate(['albums', clickedAlbum.id]);
- };
+  goToDetailPage(clickedAlbum) {
+   this.router.navigate(['albums', clickedAlbum.$key]);
+  }
 
  //  albums: Album[] = [
  //   new Album("Pulse", "Pink Floyd",
