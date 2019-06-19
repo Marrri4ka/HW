759e47df33543e80237bdf5c20f9fd12971cb5a0
Student <example@example.com>
2019-06-19 10:21:47 -0700

    delete and update functionality

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -13,6 +13,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class MarketplaceComponent implements OnInit {
 
   albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private albumService: AlbumService){}
 
@@ -22,6 +23,7 @@ export class MarketplaceComponent implements OnInit {
 
   goToDetailPage(clickedAlbum) {
    this.router.navigate(['albums', clickedAlbum.$key]);
+
   }
 
  //  albums: Album[] = [
